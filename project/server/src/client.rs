9ee9699fd4602b899c1fe89ac33070dcb87db042
MaximoGismondi <maximogismondi@gmail.com>
2024-04-23 22:54:12 -0300

    Simple client - server implementation with multiple connection support
    Co-authored-by: Mateo Alvarez <MateAlv@users.noreply.github.com>
    Co-authored-by: Cristhian Noriega <Cristhian-Noriega@users.noreply.github.com>

---
+++ project/server/src/client.rs
@@ -0,0 +1,37 @@
+use std::env::args;
+use std::io::stdin;
+use std::io::Write;
+use std::io::{BufRead, BufReader, Read};
+use std::net::TcpStream;
+
+static CLIENT_ARGS: usize = 3;
+
+fn main() -> Result<(), ()> {
+    let argv = args().collect::<Vec<String>>();
+    if argv.len() != CLIENT_ARGS {
+        println!("Cantidad de argumentos inválido");
+        let app_name = &argv[0];
+        println!("{:?} <host> <puerto>", app_name);
+        return Err(());
+    }
+
+    let address = argv[1].clone() + ":" + &argv[2];
+    println!("Conectándome a {:?}", address);
+
+    client_run(&address, &mut stdin()).unwrap();
+    Ok(())
+}
+
+fn client_run(address: &str, stream: &mut dyn Read) -> std::io::Result<()> {
+    let reader = BufReader::new(stream);
+    let mut socket = TcpStream::connect(address)?;
+
+    for line in reader.lines() {
+        if let Ok(line) = line {
+            println!("Enviando: {:?}", line);
+            socket.write(line.as_bytes())?;
+            socket.write("\n".as_bytes())?;
+        }
+    }
+    Ok(())
+}
---
+++ project/server/src/server.rs
@@ -0,0 +1,50 @@
+// let address = "170.0.0.001".to_owned();
+use std::env::args;
+use std::io::{BufRead, BufReader, Read};
+use std::net::TcpListener;
+use std::thread;
+
+static SERVER_ARGS: usize = 2;
+
+fn main() -> Result<(), ()> {
+    let argv = args().collect::<Vec<String>>();
+    if argv.len() != SERVER_ARGS {
+        println!("Cantidad de argumentos inválido");
+        let app_name = &argv[0];
+        println!("Usage:\n{:?} <puerto>", app_name);
+        return Err(());
+    }
+
+    let address = "127.0.0.1:".to_owned() + &argv[1];
+    server_run(&address).unwrap();
+    Ok(())
+}
+
+fn server_run(address: &str) -> std::io::Result<()> {
+    let listener = TcpListener::bind(address)?;
+
+    for stream in listener.incoming() {
+        if let Ok(mut stream) = stream {
+            let address = stream.peer_addr().unwrap().to_string();
+            println!("Nueva conexión: {:?}", address);
+
+            thread::spawn(move || {
+                handle_client(&mut stream, &address).unwrap();
+            });
+        }
+    }
+
+    Ok(())
+}
+
+fn handle_client(stream: &mut dyn Read, address: &str) -> std::io::Result<()> {
+    let reader = BufReader::new(stream);
+    let mut lines = reader.lines();
+
+    while let Some(Ok(line)) = lines.next() {
+        println!("Mensaje recibidio desde el cliente: {:?}", address);
+        println!("Contenido: {:?}", line);
+    }
+
+    Ok(())
+}
