624f40322cf4ff966ebd9f540fd0e58c7867b478
Joaquin Andresen <joaquin_andresen@icloud.com>
2024-05-02 14:58:15 -0300

    Elimino prueba de drone

--- project/drone/src/main.rs
+++ project/drone/src/main.rs
@@ -1,8 +1,3 @@
-use sauron::connect;
-
 fn main() {
-    let client_id: String = "drone1".to_string();
-    let connect_pkg = connect(client_id, false, 0, None, None);
-    let bytes_vec = connect_pkg.to_bytes();
-    println!("{:?}", bytes_vec)
+    println!("Hello, world!");
 }
