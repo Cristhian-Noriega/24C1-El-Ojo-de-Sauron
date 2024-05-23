b97dca211a755d85bd6e6d123752cc080d523d20
Joaquin Andresen <joaquin_andresen@icloud.com>
2024-05-23 19:53:34 -0300

    Changed gtk to egui

--- project/monitor/src/main.rs
+++ project/monitor/src/main.rs
@@ -2,6 +2,16 @@ use crate::ui::ui_application::UIApplication;
 
 pub mod ui;
 
-fn main() {
-    UIApplication::start();
+fn main() -> Result<(), eframe::Error>{
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default(),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Monitor",
+        options,
+        Box::new(|_cc| {
+            Box::<UIApplication>::default()
+        }),
+    )
 }
--- project/monitor/src/ui/ui_application.rs
+++ project/monitor/src/ui/ui_application.rs
@@ -1,57 +1,30 @@
-use gtk::prelude::*;
-use gtk::{Builder, Button, Label, Window};
+use eframe::egui;
 
 pub struct UIApplication {
-    builder: Builder,
+    response: String,
 }
 
 impl Default for UIApplication {
     fn default() -> Self {
-        if let Err(err) = gtk::init() {
-            eprintln!("Error initializing GTK: {}", err);
+        Self {
+            response: "no response".to_owned(),
         }
-        let builder = Builder::from_file("monitor/src/ui/view.glade");
-        UIApplication { builder }
     }
 }
 
-impl UIApplication {
-    pub fn start() {
-        let mut window = UIApplication::default();
-        window.show_window();
-    }
-
-    pub fn show_window(&mut self) {
-        let window: Option<Window> = self.builder.get_object("window");
-
-        println!("{:?}", window);
-
-        if let Some(w) = window {
-            self.connect_button();
-            w.show_all();
-        };
-        gtk::main();
-    }
-
-    fn connect_button(&mut self) {
-        let button_connect: Option<Button> = self.builder.get_object("button_connect");
-        let builder = self.builder.clone();
-
-        println!("{:?}", button_connect);
-
-        if let Some(b) = button_connect {
-            b.connect_clicked(move |_| {
-                // Aca deberia realizar el connect y obtener la respuesta
-                Self::set_label(&builder, "label_response", "Connected!");
+impl eframe::App for UIApplication {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Monitor");
+            ui.horizontal(|ui| {
+                if ui.button("Connect").clicked() {
+                    self.response = "Connected!".to_owned();
+                }
+                ui.vertical_centered(|ui| {
+                    ui.label("Response");
+                    ui.label(format!("{}", self.response));
+                });
             });
-        }
+        });
     }
-
-    fn set_label(builder: &Builder, key: &str, value: &str) {
-        let label: Option<Label> = builder.get_object(key);
-        if let Some(l) = label {
-            l.set_text(value);
-        }
-    }
-    
 }
