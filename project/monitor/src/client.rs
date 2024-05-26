1fddd4dbca48c51fd54147256cb87c71ad7733ce
MaximoGismondi <maximogismondi@gmail.com>
2024-05-26 18:50:29 -0300

    cippy fix

--- project/monitor/src/client.rs
+++ project/monitor/src/client.rs
@@ -1,18 +1,16 @@
+#![allow(clippy::unused_io_amount)]
+
+use sauron::model::{
+    components::encoded_string::EncodedString, components::qos::QoS,
+    components::topic_level::TopicLevel, components::topic_name::TopicName, packet::Packet,
+    packets::connect::Connect, packets::publish::Publish,
+};
+use std::io::Read;
+use std::io::Write;
+use std::net::TcpStream;
 use std::sync::mpsc::Sender;
 use std::sync::{Arc, Mutex};
 use std::{env::args, thread};
-use std::io::Read;
-use std::net::TcpStream;
-use std::io::Write;
-use sauron::model::{
-    packet::Packet,
-    packets::connect::Connect,
-    packets::publish::Publish,
-    components::encoded_string::EncodedString,
-    components::qos::QoS,
-    components::topic_level::TopicLevel,
-    components::topic_name::TopicName,
-};
 
 static CLIENT_ARGS: usize = 3;
 
@@ -39,20 +37,20 @@ impl Client {
             response_text: Arc::new(Mutex::new("".to_owned())),
             address,
             to_server_stream: Arc::new(Mutex::new(None)),
-            sender
+            sender,
         }
     }
 
     pub fn connect_to_server(&self) -> std::io::Result<TcpStream> {
         println!("Conectándome a {:?}", self.address);
         let mut to_server_stream = TcpStream::connect(&self.address)?;
-    
+
         let client_id_bytes: Vec<u8> = b"monitor".to_vec();
         let client_id = EncodedString::new(client_id_bytes);
         let will = None;
         let login = None;
         let connect_package = Connect::new(false, 0, client_id, will, login);
-    
+
         let _ = to_server_stream.write(connect_package.to_bytes().as_slice());
         // update the field to_server_stream
         *self.to_server_stream.lock().unwrap() = Some(to_server_stream.try_clone()?);
@@ -62,7 +60,7 @@ impl Client {
 
     pub fn client_run(&mut self) -> std::io::Result<()> {
         let to_server_stream = self.connect_to_server()?;
-    
+
         let mut to_server_stream_clone = to_server_stream.try_clone()?;
         let connection_status = Arc::clone(&self.connection_status);
         let response_text = Arc::clone(&self.response_text);
@@ -81,16 +79,15 @@ impl Client {
                                     connack.session_present()
                                 );
                                 *connection_status.lock().unwrap() = "connected".to_owned();
-                                *response_text.lock().unwrap() = format!("{}",connack);
+                                *response_text.lock().unwrap() = format!("{}", connack);
                             }
                             Packet::Publish(publish) => {
                                 println!("Received Publish packet {:?}", publish);
-    
+
                                 let message = publish.message();
                                 let message_str = String::from_utf8_lossy(message).to_string();
-    
-                                println!("Message: {:?}", message_str);
 
+                                println!("Message: {:?}", message_str);
                             }
                             Packet::Puback(puback) => {
                                 println!("Received Puback packet {:?}", puback);
@@ -125,15 +122,14 @@ impl Client {
                 }
             }
         });
-    
+
         Ok(())
     }
 
-
     pub fn publish(&self, topic: &str, message: &str) -> std::io::Result<()> {
         let mut levels = vec![];
-        let  message = message.trim();
-    
+        let message = message.trim();
+
         for level in topic.split(' ') {
             if let Ok(TopicLevel::Literal(literal)) =
                 TopicLevel::from_bytes(level.as_bytes().to_vec())
@@ -141,14 +137,14 @@ impl Client {
                 levels.push(literal);
             }
         }
-    
+
         let dup = false;
         let qos = QoS::AtLeast;
         let retain = false;
         let package_identifier = Some(1);
         let topic_name = TopicName::new(levels, false);
         let message_bytes = message.as_bytes().to_vec();
-    
+
         let publish_packet = Publish::new(
             dup,
             qos,
@@ -157,12 +153,17 @@ impl Client {
             package_identifier,
             message_bytes,
         );
-    
+
         println!("Packet Publish: {:?}", publish_packet);
-        let _ = self.to_server_stream.lock().unwrap().as_mut().unwrap().write(publish_packet.to_bytes().as_slice());
+        let _ = self
+            .to_server_stream
+            .lock()
+            .unwrap()
+            .as_mut()
+            .unwrap()
+            .write(publish_packet.to_bytes().as_slice());
         println!("Sent Publish packet");
-    
+
         Ok(())
     }
-    
 }
