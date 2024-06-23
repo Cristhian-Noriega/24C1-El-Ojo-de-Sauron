6996d18f07ee51c0c4eb830f6103283f6d3e1105
Joaquin Andresen <joaquin_andresen@icloud.com>
2024-06-23 13:36:30 -0300

    Actualizo tests de integracion

--- project/tests-integration/tests/integration_tests.rs
+++ project/tests-integration/tests/integration_tests.rs
@@ -1,5 +1,9 @@
 use monitor::monitor::Monitor;
 use common::incident::{Incident, IncidentStatus};
+use drone::drone::Drone;
+use drone::drone_status::{DroneStatus, TravelLocation};
+use camera_system::camera_system::CameraSystem;
+use camera_system::camera::Camera;
 
 #[test]
 fn test_new_incident() {
@@ -8,14 +12,40 @@ fn test_new_incident() {
         "incident1".to_string(),
         "incident1".to_string(),
         "incident1".to_string(),
-        1.0,
-        1.0,
+        2.0,
+        2.0,
         IncidentStatus::Pending,
     );
 
+    // Monitor
     monitor.new_incident(incident.clone());
     assert_eq!(monitor.get_incident(&incident.uuid).unwrap(), &incident);
-}
+
+    // Drone
+    let mut drone = Drone::new(1, 1.0, 1.0, 1.0, 1.0, 1.0, 5.0);
+    assert_eq!(drone.data(), "1;1;0;100");
+    drone.set_incident(Some(incident.clone()));
+    assert_eq!(drone.incident().unwrap(), incident);
+    drone.set_status(DroneStatus::Travelling(TravelLocation::Incident));
+    assert_eq!(drone.status(), DroneStatus::Travelling(TravelLocation::Incident));
+    drone.travel_to(2.0, 2.0);
+    assert_eq!(drone.data(), "1.7071067811865475;1.7071067811865475;4;99");
+
+    // Camara
+    let mut camera_system = CameraSystem::new();
+    let camera = Camera::new(
+        1 as u8,
+        1.5,
+        1.5,
+        3.0,
+    );
+    camera_system.add_camera(camera);
+    let camera_data1 = camera_system.cameras_data();
+    camera_system.new_incident(incident.clone());
+    let camera_data2 = camera_system.cameras_data();
+    assert_eq!(camera_data1, "1;1.5;1.5;0");
+    assert_eq!(camera_data2, "1;1.5;1.5;1");
+}  
 
 #[test]
 fn test_attending_incident_once() {
