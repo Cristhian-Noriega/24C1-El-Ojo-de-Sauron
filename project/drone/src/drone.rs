2863bf2a5f4517d6e97dcde44acef1b31ad1ede0
MaximoGismondi <maximogismondi@gmail.com>
2024-06-24 01:19:17 -0300

    Velocidades y estado inicial

--- project/drone/src/drone.rs
+++ project/drone/src/drone.rs
@@ -39,7 +39,7 @@ impl Drone {
             id,
             x_coordinate: x_central,
             y_coordinate: y_central,
-            status: DroneStatus::Free,
+            status: DroneStatus::Travelling(TravelLocation::Anchor),
             battery: MAXIMUM_BATTERY_LEVEL,
             x_central,
             y_central,
