8a36eecd47cdcc2b04dc3d2a71cab9d8ffa1eb16
Joaquin Andresen <joaquin_andresen@icloud.com>
2024-07-13 16:57:12 -0300

    Actualizo is_incident para que solo incluya ciertos labels

--- project/aws-demo/src/main.rs
+++ project/aws-demo/src/main.rs
@@ -26,8 +26,6 @@ async fn main() {
 async fn is_incident(s3_client: &aws_sdk_s3::Client, rekognition_client: &aws_sdk_rekognition::Client, bucket: &str, key: &str, file_path: &str) -> bool{
     let _ = upload_file(&s3_client, bucket, key, file_path).await;
 
-    let incident_labels = vec!["Vandalism", "Weapon", "Gun", "Fighting", "Knife"];
-
     let s3_image = ImageBuilder::default().s3_object({
         S3Object::builder()
             .bucket(bucket.to_string())
@@ -35,22 +33,30 @@ async fn is_incident(s3_client: &aws_sdk_s3::Client, rekognition_client: &aws_sd
             .build()
     }).build();
 
+    let general_labels_input = aws_sdk_rekognition::types::GeneralLabelsSettings::builder()
+        .label_inclusion_filters("Vandalism")
+        .label_inclusion_filters("Weapon")
+        .label_inclusion_filters("Gun")
+        .label_inclusion_filters("Fighting")
+        .label_inclusion_filters("Knife")
+        .build();
+
+    let settings_input = aws_sdk_rekognition::types::DetectLabelsSettings::builder()
+        .general_labels(general_labels_input)
+        .build();
+
     let request = rekognition_client.detect_labels()
         .image(s3_image)
-        .max_labels(10)
-        .min_confidence(10.0);
+        .settings(settings_input);
 
     let response = request.send().await;
 
     if let Ok(response) = response {
         for label in response.labels(){
-            match (label.name(), label.confidence()) {
-                (Some(name), Some(confidence)) => {
-                    if incident_labels.contains(&name) && confidence > 50.0 {
-                        return true;
-                    }
+            if let Some(confidence) = label.confidence(){
+                if confidence > 50.0 {
+                    return true;
                 }
-                _ => return false
             }
         }
         return false;
@@ -69,8 +75,6 @@ async fn upload_file(client: &aws_sdk_s3::Client, bucket: &str, key: &str, file_
         .body(body.unwrap())
         .send()
         .await?;
-
-    println!("The file was uploaded successfully");
 
     Ok(())
 }
